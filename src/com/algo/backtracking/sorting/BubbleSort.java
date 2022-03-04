914b651496cf8dde437e84fb02e15ed3c3e3cf3e
uvcourses@gmail.com <uvcourses@gmail.com>
2022-03-04 10:36:03 -0600

    Insertion sort

--- src/com/algo/backtracking/sorting/BubbleSort.java
+++ src/com/algo/backtracking/sorting/BubbleSort.java
@@ -2,9 +2,12 @@ package com.algo.backtracking.sorting;
 
 public class BubbleSort {
     public void bubbleSort(int arr[]) {
+        int count=0;
         for (int i = 0; i < arr.length; i++) {
             for (int red = arr.length - 1; red > i; red--) {
+
                 if (arr[red - 1] > arr[red]) {
+                    count++;
                     swap(arr, red, red - 1);
                 }
             }
@@ -12,6 +15,7 @@ public class BubbleSort {
         for (int j = 0; j < arr.length; j++) {
             System.out.println(arr[j]);
         }
+        System.out.println("count "+count);
     }
 
     private void swap(int[] input, int s, int t) {
@@ -22,6 +26,6 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         BubbleSort bubbleSort = new BubbleSort();
-        bubbleSort.bubbleSort(new int[]{1,2,3,4,5,6,7,8,9,0});
+        bubbleSort.bubbleSort(new int[]{10,9,8,7,6,5,4,3,2,1});
     }
 }
---
+++ src/com/algo/backtracking/sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package com.algo.backtracking.sorting;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        InsertionSort insertionSort = new InsertionSort();
+        insertionSort.insertionSort(new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1});
+    }
+
+    private void insertionSort(int[] ints) {
+
+        for (int i = 0; i < ints.length; i++) {
+            int temp = ints[i];
+            int red = i - 1;
+            while (red >= 0 && ints[red] > temp) {
+                ints[red + 1] = ints[red];
+                red--;
+            }
+            ints[red + 1] = temp;
+        }
+
+        for (int j = 0; j < ints.length; j++) {
+            System.out.println(ints[j]);
+        }
+    }
+}
