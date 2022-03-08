b0a0f8eb064858bf21deda5737d3a03781f87625
uvcourses@gmail.com <uvcourses@gmail.com>
2022-03-08 00:46:43 -0600

    weight difference between all pairs

---
+++ src/com/amazon/WeightsCount.java
@@ -0,0 +1,31 @@
+package com.amazon;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class WeightsCount {
+    public int countSegment(List<Integer> weights, int k) {
+        int result = 0;
+        for (int i = 0; i < weights.size(); i++) {
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            int j = i;
+            while (j < weights.size()) {
+                min = Math.min(weights.get(j), min);
+                max = Math.max(max, weights.get(j));
+                j++;
+                if (max - min <= k) {
+                    result++;
+                }
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        WeightsCount weightsCount = new WeightsCount();
+        int output = weightsCount.countSegment(Arrays.asList(1, 10, 2), 9);
+        System.out.println(output);
+    }
+}
